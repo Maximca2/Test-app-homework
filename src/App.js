a00d1384c203940cfb3cc6a748453d32dd722595
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-05 17:45:00 +0300

    add wizard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
-import CreateTest from './pages/CreateSomeTest/CreateTest';
+import CreateTest from './pages/CreateSomeTest/TestSetup';
 import OurTest from './pages/OurTest/OurTest';
-
+import { Wizard, useWizard } from 'react-use-wizard';
+import Steps1 from './pages/Steps/Steps1';
+import Steps2 from './pages/Steps/Steps2';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <OurTest/>
-     <CreateTest/>
+
+      <Wizard>
+        <CreateTest />
+        <OurTest />
+        
+      </Wizard>
+
+
+
     </div>
   );
 }
