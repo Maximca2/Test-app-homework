f19984b6861c15e8ceee8c1825e39dad02696f42
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-05 11:16:28 +0300

    add OurTest and createTest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import CreateTest from './pages/CreateSomeTest/CreateTest';
+import OurTest from './pages/OurTest/OurTest';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <OurTest/>
+     <CreateTest/>
     </div>
   );
 }
