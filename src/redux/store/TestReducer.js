b38f1bff1d0ae0b01a33fa36eeda67735df00f13
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-08 11:31:56 +0300

    fix problem wit rerendering in my state i add random sort and add takeNumberOfQuestions

--- src/redux/store/TestReducer.js
+++ src/redux/store/TestReducer.js
@@ -1,20 +1,38 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 
-import { FETCH_CUR_TEST } from "./actions";
+import { FETCH_CUR_TEST, TAKE_NUMBER_OF_QUESTIONS } from "./actions";
 
 export const fetchCurTest = createAction(FETCH_CUR_TEST)
+export const takeNumberOfQuestions = createAction(TAKE_NUMBER_OF_QUESTIONS)
 
 const defaultState = {
     tests: [],
+    numberOfQuestions: [],
+    ourQuestions:[]
 }
 //Reducer 
 export const testReducer = createReducer(defaultState, {
 
     [fetchCurTest]: function (state, { payload }) {
-        const {results} = payload;
+        const { results } = payload;
         
+        results.forEach(it => {
+            const{incorrect_answers,correct_answer} = it
+            incorrect_answers.push(correct_answer)
+            const randomQuestions = incorrect_answers
+                .flat()
+                .sort(() => (Math.random() > 0.5 ? 1 : -1));
+                state.ourQuestions.push([randomQuestions])
+        })
+
         state.tests.push(results.flat())
 
     },
-    
+    [takeNumberOfQuestions]: function (state, { payload }) {
+
+
+        state.numberOfQuestions.push(Number(payload))
+
+    },
+
 })
